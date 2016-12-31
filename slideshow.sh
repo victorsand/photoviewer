763120586b8070d5bddbae0e1c672ff5d751b825
Victor Sand <victor.sand@gmail.com>
2016-12-31 09:07:18 +0100

    readme, feh timing

--- slideshow.sh
+++ slideshow.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo killall feh
 /usr/bin/python /home/pi/apps/photoview/fetch.py
-feh -ZF -D 30 --hide-pointer /home/pi/apps/photoview/images
+feh -ZF -D 600 --hide-pointer /home/pi/apps/photoview/images
