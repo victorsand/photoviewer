7cfdda3d8afdb43e4ba7e0704759183356451c9a
Victor Sand <victor.sand@gmail.com>
2017-09-11 20:11:02 +0200

    ulla

--- fetch.py
+++ fetch.py
@@ -1,14 +1,15 @@
 import os
 import os.path
 import boto3
+import logging
 
 def sync_image_folder():
 
 	app_path = os.path.join(os.path.sep, 'home', 'pi', 'apps', 'photoviewer')
-	print ('App path: %s' % app_path)
+	logging.info('App path: %s' % app_path)
 
 	config_file_path = os.path.join(app_path, 'config', 'aws')
-	print ('Config file: %s' % config_file_path)	
+	logging.info('Config file: %s' % config_file_path)	
 
 	with open(config_file_path) as config:
 		config_lines = config.readlines()
@@ -23,7 +24,7 @@ def sync_image_folder():
 		os.makedirs(image_path)
 
 	files_in_dir = os.listdir(image_path)
-	print ('Files in dir: %s ' % files_in_dir)
+	logging.info('Files in dir: %s ' % files_in_dir)
 
 	client = boto3.client(
 		's3',
@@ -32,25 +33,35 @@ def sync_image_folder():
 		aws_secret_access_key=secret_access_key
 	)
 
-	print ('Connecting to bucket: %s' % bucket_name)
+	logging.info('Connecting to bucket: %s' % bucket_name)
 
 	bucket_objects = client.list_objects(Bucket=bucket_name)['Contents']
 	files_in_bucket = [key['Key'] for key in bucket_objects]
-	print ('Files in bucket: %s' % files_in_bucket)
+	logging.info('Files in bucket: %s' % files_in_bucket)
 
 	for bucket_file in files_in_bucket:
 		if not bucket_file in files_in_dir:
-			print ('Downloading %s ' % bucket_file)
+			logging.info('Downloading %s ' % bucket_file)
 			file_path = os.path.join(image_path, bucket_file)
 			client.download_file(bucket_name, bucket_file, file_path)
 		else:
-			print ('%s already downloaded' % bucket_file)
+			logging.info('%s already downloaded' % bucket_file)
 
 	for downloaded_file in files_in_dir:
 		if not downloaded_file in files_in_bucket:
-			print ('Removing % s ' % downloaded_file)
+			logging.info('Removing % s ' % downloaded_file)
 			os.remove(os.path.join(image_path, downloaded_file))
 	
 	return
 
-sync_image_folder()
+
+
+logging.basicConfig(filename='/home/pi/apps/photoviewer/debug.log',
+		    level=logging.INFO,
+		    format='%(asctime)s %(name)-8s %(levelname)-8s %(message)s',
+		    datefmt='%Y-%m-%d %H:%M:%S')
+logging.info('Running fetch script')
+try:
+	sync_image_folder()
+except Exception as e:
+	logging.exception(e)
