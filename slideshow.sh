b09ca1e47233c269930ba8d80667e775a95dfa6e
Victor Sand <victor.sand@gmail.com>
2018-12-22 09:55:58 +0100

    random order

--- slideshow.sh
+++ slideshow.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo killall feh
 /usr/bin/python /home/pi/apps/photoviewer/fetch.py
-feh -ZF -D 600 --hide-pointer /home/pi/apps/photoviewer/images
+feh -ZFz -D 600 --hide-pointer /home/pi/apps/photoviewer/images
